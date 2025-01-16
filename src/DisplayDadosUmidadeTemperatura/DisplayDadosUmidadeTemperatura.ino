6411b4475f2353397e37055405c92c6c716f1473
HeytorPires <heytor.cacho29@gmail.com>
2025-01-15 22:58:30 -0300

    LCD com temperatura

--- src/DisplayDadosUmidadeTemperatura/DisplayDadosUmidadeTemperatura.ino
+++ src/DisplayDadosUmidadeTemperatura/DisplayDadosUmidadeTemperatura.ino
@@ -1,3 +1,4 @@
+#include "dht.h" //INCLUSÃO DE BIBLIOTECA
 #include <Wire.h>                // Biblioteca para comunicação I2C
 #include <LiquidCrystal_I2C.h>   // Biblioteca para controle do display I2C
 
@@ -7,30 +8,42 @@
 
 LiquidCrystal_I2C lcd(ende, col, lin); // Inicializa o display I2C com o endereço e dimensões
 
-void setup() {  
+const int pinoDHT11 = A2; //PINO ANALÓGICO UTILIZADO PELO DHT11
+ 
+dht DHT; //VARIÁVEL DO TIPO DHT
+ 
+void setup(){
   lcd.init();        // Inicia o display
   lcd.backlight();   // Liga a luz de fundo do display
   lcd.clear();       // Limpa o display
+  Serial.begin(9600); //INICIALIZA A SERIAL
 }
+ 
+void loop(){
+  DHT.read11(pinoDHT11); //LÊ AS INFORMAÇÕES DO SENSOR
+  Serial.print("Umidade: "); //IMPRIME O TEXTO NA SERIAL
+  Serial.print(DHT.humidity); //IMPRIME NA SERIAL O VALOR DE UMIDADE MEDIDO
+  Serial.print("%"); //ESCREVE O TEXTO EM SEGUIDA
+  Serial.print(" / Temperatura: "); //IMPRIME O TEXTO NA SERIAL
+  Serial.print(DHT.temperature, 0); //IMPRIME NA SERIAL O VALOR DE UMIDADE MEDIDO E REMOVE A PARTE DECIMAL
+  Serial.println("*C"); //IMPRIME O TEXTO NA SERIAL
 
-void loop() {
-  // Linha 1
+  delay(2000); //INTERVALO DE 2 SEGUNDOS * NÃO DIMINUIR ESSE VALOR
   lcd.setCursor(0, 0);         // Define o cursor na primeira coluna e primeira linha
-  lcd.print("MILENA ");        // Imprime a mensagem na primeira linha
-  delay(2000);                 // Aguarda 2 segundos
+  lcd.print(DHT.temperature);        // Imprime a mensagem na primeira linha
 
   // Linha 2
   lcd.setCursor(0, 1);         // Define o cursor na primeira coluna e segunda linha
-  lcd.print("EU TE AMO");      // Imprime a mensagem na segunda linha
+  lcd.print("Graus Celcius");      // Imprime a mensagem na segunda linha
   delay(2000);                 // Aguarda 2 segundos
   
   lcd.clear();                 // Limpa o display para novas mensagens
   
   // Alternando mensagens
   lcd.setCursor(0, 0);
-  lcd.print("TOTOI TE AMA");
+  lcd.print(DHT.humidity);
   lcd.setCursor(0, 1);
-  lcd.print("MILENINHA!");
+  lcd.print("Umidade no ar");
   delay(2000);
 
   lcd.clear(); // Limpa o display
